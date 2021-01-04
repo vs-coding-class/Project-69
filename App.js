4a497225e03322f1cfbbb2395cffa0be4ba0e8fd
vs-coding-class <65456951+vs-coding-class@users.noreply.github.com>
2021-01-04 17:21:41 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { View } from 'react-native';
+import ScanScreen from './screens/ScanScreen';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View>
+        <ScanScreen/>
+      </View>
+    );
+  }
+}
